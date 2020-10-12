74777f667bf17557f8f79822de7cbb4aee29e00a
atronah <atronah.ds@gmail.com>
2020-10-12 20:03:55 +0300

    ft(bot): adds configurable logging

--- bot.py
+++ bot.py
@@ -1,17 +1,54 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 import yaml
-import logging
+import logging, logging.config
 import sys
 
 # default settings
-settings = {'access': {
-                'token': None
-                }
+settings = {
+    'access': {
+        'token': None
+    },
+    'logging': {
+        'version': 1.0,
+        'formatters': {
+            'default': {
+                'format': '[{asctime}]{levelname: <5}({name}): {message}',
+                'style': '{'
             }
+        },
+        'handlers': {
+            'general': {
+                'class': 'logging.handlers.WatchedFileHandler',
+                'level': 'INFO',
+                'filename': 'bot.log',
+                'formatter': 'default'
+            },
+            'unknown_messages': {
+                'class': 'logging.handlers.WatchedFileHandler',
+                'level': 'DEBUG',
+                'filename': 'unknown_messages.log',
+                'formatter': 'default'
+            }
+        },
+        'loggers': {
+            'unknown_messages': {
+                'level': 'DEBUG',
+                'handlers': ['unknown_messages']
+            }
+        },
+        'root': {
+            'level': 'INFO',
+            'handlers': ['general']
+        },
+    }
+}
 
 with open('conf.yml') as conf:
     settings.update(yaml.safe_load(conf))
+
+
+logging.config.dictConfig(settings['logging'])
 
 if not settings['access']['token']:
     logging.error('Empty bot token in conf.yml (`access/token`')
