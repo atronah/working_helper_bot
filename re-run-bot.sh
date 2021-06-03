40dceae41ed3c3a6f43ac5bc3367ccd8c38cc121
atronah <atronah.ds@gmail.com>
2021-06-03 17:29:26 +0300

    fx(re-run-bot): improve script to re-run bot
    - adds shebang
    - use full path to script to run and kill to prevent name conflict with other processes who has`bot.py` as main executable py-script
    - adds initialize python virtual env in `venv` folder
    - adds updating pip and setuptools

--- re-run-bot.sh
+++ re-run-bot.sh
@@ -1,4 +1,19 @@
-pkill -f 'python3 bot.py'
-source venv/bin/activate
+#! /usr/bin/env bash
+
+script_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+pkill -f "python3 ${script_path}/bot.py"
+
+pushd "${script_path}"
+
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
+source ./venv/bin/activate
+pip install --upgrade pip setuptools
 pip install .
-python3 bot.py
+# full path to prevent killing any other bots with `bot.py` main script
+nohup python3 "${script_path}/bot.py" &
+
+popd
