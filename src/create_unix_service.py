855c9e85a12cf6d36f7b7ea3fd900675b7e9b246
atronah <atronah.ds@gmail.com>
2021-07-03 13:24:18 +0300

    adds ability to specify service name

--- src/create_unix_service.py
+++ src/create_unix_service.py
@@ -8,9 +8,20 @@ else:
 
 
 def main():
+    service_name = sys.argv[1] if len(sys.argv) > 1 else 'work_assistant_bot'
+    service_dir = '/etc/systemd/system'
+    service_ext = '.service'
+    
+    service_path = os.path.join(service_dir, service_name + service_ext)
+    
+    if os.path.exists(service_path):
+        print(f'{service_path} already exists')
+        sys.exit(1)
+    
     package_info = metadata('work_assistant')
     working_directory = os.path.abspath('.')
-    with open('work_assistant_bot.service', 'w') as srvc:
+    
+    with open(service_path, 'w') as srvc:
         srvc.write(os.linesep.join([
             "[Unit]",
             f"Description={package_info['Summary']}",
